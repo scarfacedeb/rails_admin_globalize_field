3b480b38cf0723c0c46c70e98305356e661df059
scarfacedeb <scarfacedeb@gmail.com>
2019-09-14 17:00:35 +0300

    Refactor field class and view
    Why:
    
    To simplify the code and view

--- lib/rails_admin_globalize_field.rb
+++ lib/rails_admin_globalize_field.rb
@@ -1,19 +1,15 @@
+# frozen_string_literal: true
+
 require 'rails_admin_globalize_field/engine'
-require 'rails_admin'
 require 'rails_admin/config/fields/association'
+require 'rails_admin_globalize_field/tab'
 
 module RailsAdmin
   module Config
     module Fields
       module Types
         class GlobalizeTabs < RailsAdmin::Config::Fields::Association
-          RailsAdmin::Config::Fields::Types::register(:globalize_tabs, self)
-
-          # A workaround for https://github.com/sferik/rails_admin/pull/1726
-          def initialize(parent, name, properties)
-            properties = parent.abstract_model.associations.detect { |p| name == p.name }
-            super
-          end
+          RailsAdmin::Config::Fields::Types.register(:globalize_tabs, self)
 
           register_instance_option :partial do
             :form_globalize_tabs
@@ -23,36 +19,33 @@ module RailsAdmin
             "#{super}_attributes".to_sym
           end
 
-          def generic_help
-            "" # false is ignored by I18n.translate
-          end
-
-          # Reader for validation errors of the bound object
-          def errors
-            bindings[:object].errors[name]
-          end
-
           def available_locales
             I18n.available_locales
           end
-          def current_locale
-            I18n.locale
+
+          def tabs
+            tabs =
+              available_locales.map do |locale|
+                RailsAdminGlobalizeField::Tab.new(
+                  locale,
+                  bindings[:object].translation_for(locale),
+                  validate: submit_action?
+                )
+              end
+
+            activate_tab(tabs)
+            tabs
           end
 
-          # Returns array of Translation objects.
-          # It gets existing or creates new empty translation for every locale.
-          # Call the first time with reset_cache == true to update memoized translations.
-          def translations reset_cache=false
-            return @translations if @translations && !reset_cache
+          # Hack to distinguish between REST new/edit and create/update actions.
+          # Taken from rails_admin/configs/new.rb
+          def submit_action?
+            !bindings[:controller].request.get?
+          end
 
-            translations = @bindings[:object].translations_by_locale
-            new_locales = available_locales - translations.keys.map(&:to_sym)
-
-            new_locales.map do |locale|
-              translations[locale] = @bindings[:object].translations.new({ locale: locale })
-            end
-
-            @translations = translations
+          def activate_tab(tabs)
+            opened = tabs.find(&:invalid?) || tabs.first
+            opened&.active!
           end
         end
       end
---
+++ lib/rails_admin_globalize_field/tab.rb
@@ -0,0 +1,31 @@
+module RailsAdminGlobalizeField
+  class Tab
+    attr_reader :locale, :translation
+
+    def initialize(locale, translation, validate: true)
+      @locale = locale
+      @translation = translation
+      @validate = validate
+    end
+
+    def id
+      ['pane', translation.model_name.param_key, locale].join('-')
+    end
+
+    def active!
+      @active = true
+    end
+
+    def active?
+      @active
+    end
+
+    def valid?
+      !@validate || translation.valid?
+    end
+
+    def invalid?
+      !valid?
+    end
+  end
+end
