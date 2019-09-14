c63efe2d12f74aace636f118766a4269d55270ec
scarfacedeb <scarfacedeb@gmail.com>
2019-09-14 17:06:13 +0300

    Move field class from RailsAdmin into gem namespace

--- lib/rails_admin_globalize_field.rb
+++ lib/rails_admin_globalize_field.rb
@@ -1,54 +1,7 @@
 # frozen_string_literal: true
 
 require 'rails_admin_globalize_field/engine'
-require 'rails_admin/config/fields/association'
-require 'rails_admin_globalize_field/tab'
+require 'rails_admin_globalize_field/globalize_tabs'
 
-module RailsAdmin
-  module Config
-    module Fields
-      module Types
-        class GlobalizeTabs < RailsAdmin::Config::Fields::Association
-          RailsAdmin::Config::Fields::Types.register(:globalize_tabs, self)
-
-          register_instance_option :partial do
-            :form_globalize_tabs
-          end
-
-          def method_name
-            "#{super}_attributes".to_sym
-          end
-
-          def available_locales
-            I18n.available_locales
-          end
-
-          def tabs
-            tabs =
-              available_locales.map do |locale|
-                RailsAdminGlobalizeField::Tab.new(
-                  locale,
-                  bindings[:object].translation_for(locale),
-                  validate: submit_action?
-                )
-              end
-
-            activate_tab(tabs)
-            tabs
-          end
-
-          # Hack to distinguish between REST new/edit and create/update actions.
-          # Taken from rails_admin/configs/new.rb
-          def submit_action?
-            !bindings[:controller].request.get?
-          end
-
-          def activate_tab(tabs)
-            opened = tabs.find(&:invalid?) || tabs.first
-            opened&.active!
-          end
-        end
-      end
-    end
-  end
+module RailsAdminGlobalizeField
 end
